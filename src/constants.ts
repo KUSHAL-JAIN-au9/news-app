82d235927ad791a466a92b69096e2882f3086a70
Kushal Jain <jkushal069@gmail.com>
2025-01-20 05:45:32 +0530

    added proxy url

--- src/constants.ts
+++ src/constants.ts
@@ -2,18 +2,20 @@ const NEWS_API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 const NYT_API_KEY = process.env.REACT_APP_NYT_API_KEY;
 const GUARDIAN_API_KEY = process.env.REACT_APP_GUARDIAN_API_KEY;
 
+export const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+
 export const apiConfigs = [
   {
     key: 'newsAPI',
-    url: `https://newsapi.org/v2/everything?q=keyword&apiKey=${NEWS_API_KEY}`,
+    url: `${PROXY_URL}https://newsapi.org/v2/everything?q=keyword&apiKey=${NEWS_API_KEY}`,
   },
   {
     key: 'gaurdianAPI',
-    url: `https://content.guardianapis.com/search?api-key=${GUARDIAN_API_KEY}`,
+    url: `${PROXY_URL}https://content.guardianapis.com/search?api-key=${GUARDIAN_API_KEY}`,
   },
   {
     key: 'nytAPI',
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${NYT_API_KEY}`,
+    url: `${PROXY_URL}https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${NYT_API_KEY}`,
   },
 ];
 
